faaf6fa29a64abcbf509ef9bffc7eb19ed01d21d
salman_ghannam1 <sloome222@gmail.com>
2025-10-13 23:02:49 +0300

    f

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,11 +3,10 @@
 /**
  * main - Entry point
  *
- * Description: prints "Programming is like building a multilingual puzzle"
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
