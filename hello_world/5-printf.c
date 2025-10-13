06da91b3bf95f8a99f7823d372c28ee318e6dd12
salman_ghannam1 <sloome222@gmail.com>
2025-10-13 22:19:43 +0300

    f

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: Prints "with proper grammar, but the outcome is a piece of art,"
- * followed by a new line using printf.
+ * Description: prints "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line using printf function.
  *
  * Return: Always 0 (Success)
  */
