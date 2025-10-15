529f9d7aa42704e48dd6b1ccf65b8e06d5b37983
salman_ghannam1 <sloome222@gmail.com>
2025-10-15 19:09:57 +0300

    f

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,11 @@ void print_alphabet_x10(void)
 	int i;
 	char c;
 
-	for (i = 1; i <= 10;i ++)
+	for (i = 0; i < 10; i++)
 	{
-		for (c = 'a';c <= 'z'; c ++)
-		{
-			_putchar(c);
-		}
-		_putchar('\n');
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);   /* 1st and only use for letters */
+
+		_putchar('\n');    /* 2nd use for newline */
 	}
 }
