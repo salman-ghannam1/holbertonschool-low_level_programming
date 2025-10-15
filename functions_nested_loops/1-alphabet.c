06baba7f2e67a7d99149aedb21324f28f2a8d561
salman_ghannam1 <sloome222@gmail.com>
2025-10-15 11:35:26 +0300

    f

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,4 +3,15 @@
 /**
  * print_alphabet - prints the lowercase alphabet followed by a new line
  *
- * Return
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
