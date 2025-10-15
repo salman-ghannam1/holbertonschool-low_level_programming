a33f0b6da257ef16fd2c62aeec1daecdadff4fb4
salman_ghannam1 <sloome222@gmail.com>
2025-10-15 11:23:08 +0300

    f

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,11 +6,5 @@ void print_alphabet(void)
     for (c = 'a'; c <= 'z'; c++)
         _putchar(c);
 
-    _putchar('\n'); // اطبع سطر جديد بعد الحروف
-}
-
-int main(void)
-{
-    print_alphabet();
-    return (0);
+    _putchar('\n');
 }
