c878b32050199fd87c8702481296b26b78439643
salman_ghannam1 <sloome222@gmail.com>
2025-10-15 11:46:10 +0300

    f

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+}
