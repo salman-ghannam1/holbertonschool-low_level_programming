b4845e01286509631fa7cbdd171f79ef9ca21d5a
salman_ghannam1 <sloome222@gmail.com>
2025-10-16 14:15:49 +0300

    l

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,32 +1,12 @@
 #include "main.h"
 
-
 /**
- * main - check the print_last_digit function
+ * main - check the jack_bauer function
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int r;
-
-	r = print_last_digit(98);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-
-	r = print_last_digit(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-
-	r = print_last_digit(-1024);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-
+	jack_bauer();
 	return (0);
 }
