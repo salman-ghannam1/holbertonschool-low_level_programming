d0ad3253c00a36bfa76767d1f71205335fca1480
salman_ghannam1 <sloome222@gmail.com>
2025-10-16 16:19:05 +0300

    f

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,12 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code
+ * main - check the add function
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    times_table();
-    return (0);
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
 }
