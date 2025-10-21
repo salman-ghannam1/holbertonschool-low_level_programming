1942c00c5f7365266746501947c89da4c740cab4
salman_ghannam1 <sloome222@gmail.com>
2025-10-21 11:37:30 +0300

    f

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_numbers - prints the numbers from 0 to 9
  *
@@ -11,8 +12,6 @@ void print_numbers(void)
 	for (i = 0; i < 10; i++)
 	{
 		_putchar(i + '0');
-
-		_putchar('\n');
-		
 	}
+	_putchar('\n');
 }
