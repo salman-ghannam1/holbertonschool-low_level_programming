ab961a7a339f8de278fdedc5e1fa2b1e6e484ad3
salman_ghannam1 <sloome222@gmail.com>
2025-10-21 11:34:12 +0300

    f

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_numbers - prints the numbers from 0 to 9
  *
@@ -11,9 +10,9 @@ void print_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(i + '0');
+		_putchar(i + '0');
 
-		putchar('\n');
+		_putchar('\n');
 		
 	}
 }
