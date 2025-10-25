5025f9dead26f76f5aea1d27f206125d888b8cad
salman_ghannam1 <sloome222@gmail.com>
2025-10-25 06:39:13 +0300

    f

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,43 +1,41 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: pointer to the string
+ * _atoi - Convert a string to an integer.
+ * @s: The pointer to convert
  *
- * Return: integer value of the string, or 0 if no numbers found
+ * Return: A integer
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	int result = 0;
-	int found = 0;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	/* move through the string character by character */
-	while (s[i] != '\0')
+	while (s[c])
 	{
-		/* if a sign is found before the number */
-		if (s[i] == '-')
+		if (s[c] == 45)
 		{
-			sign *= -1;
+			min *= -1;
 		}
-		else if (s[i] == '+')
+
+		while (s[c] >= 48 && s[c] <= 57)
 		{
-			/* ignore it */
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
 		}
-		/* if the character is a digit */
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			found = 1;
-			result = (result * 10) + (s[i] - '0');
-		}
-		/* if we already started reading a number and found a non-digit */
-		else if (found)
+
+		if (isi == 1)
 		{
 			break;
 		}
-		i++;
+
+		c++;
 	}
 
-	return (sign * result);
+	ni *= min;
+	return (ni);
 }
+
