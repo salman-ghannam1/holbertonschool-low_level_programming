c8310f59ac3995b980b8e51bd8006c22133048c4
salman_ghannam1 <sloome222@gmail.com>
2025-10-26 03:30:24 +0300

    f

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,23 +1,50 @@
 #include "main.h"
 
 /**
- * _strcmp - compares two strings like the standard strcmp
- * @s1: first string
- * @s2: second string
- *
- * Return: 0 if equal; negative if s1 < s2; positive if s1 > s2
- */
+  * _strcmp - Compares two strings
+  * @s1: The first string
+  * @s2: The second string
+  *
+  * Return: int value
+  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int a = 0, b = 0, c = 0, r = 0, lim;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[a])
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		i++;
+		a++;
 	}
 
-	/* handles cases: both end (returns 0) or one is prefix of the other */
-	return (s1[i] - s2[i]);
+	while (s2[b])
+	{
+		b++;
+	}
+
+	if (a <= b)
+	{
+		lim = a;
+	}
+	else
+	{
+		lim = b;
+	}
+
+	while (c <= lim)
+	{
+		if (s1[c] == s2[c])
+		{
+			c++;
+			continue;
+		}
+		else
+		{
+			r = s1[c] - s2[c];
+			break;
+		}
+
+		c++;
+	}
+
+	return (r);
 }
