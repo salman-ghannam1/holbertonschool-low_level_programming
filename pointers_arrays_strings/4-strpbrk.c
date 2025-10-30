d36c662d24e3d9b922143abace7ca9405cf72191
salman_ghannam1 <sloome222@gmail.com>
2025-10-30 19:12:33 +0300

    f

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: the string to be scanned
+ * @accept: the string containing the characters to match
+ *
+ * Return: pointer to the first occurrence in s
+ * of any of the bytes in accept, or NULL if none found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (&s[i]);
+		}
+	}
+
+	return (NULL);
+}
