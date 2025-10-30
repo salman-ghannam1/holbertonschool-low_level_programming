7369903ea4d3183c0b43d9c0a9ec5887bc85721f
salman_ghannam1 <sloome222@gmail.com>
2025-10-30 18:45:04 +0300

    f

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to search in
+ * @c: character to locate
+ *
+ * Return: pointer to the first occurrence of c in s, or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+
+	if (s[i] == c)
+		return (&s[i]);
+
+	return (NULL);
+}
