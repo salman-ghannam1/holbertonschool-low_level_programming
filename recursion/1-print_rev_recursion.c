92c43d3acb8126a609559a3633a15e3ee9887294
salman_ghannam1 <sloome222@gmail.com>
2025-11-02 06:46:43 +0300

    f

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -3,14 +3,14 @@
 /**
  * _print_rev_recursion - prints a string in reverse
  * @s: the string to be printed
+ *
+ * Return: void
  */
-
 void _print_rev_recursion(char *s)
 {
-    if (*s != '\0')
-    {
-        _print_rev_recursion(s + 1);
-        _putchar(*s);
-    }
-    
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
