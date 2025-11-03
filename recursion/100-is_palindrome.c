f68b073ca731d48d577daae8ac6688040816b077
salman_ghannam1 <sloome222@gmail.com>
2025-11-03 04:50:57 +0300

    f

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -9,7 +9,9 @@
 int _strlen_recursion(char *s)
 {
     if (*s == '\0')
+    {
         return 0;
+    }
 
     return (1 + _strlen_recursion(s + 1));
 }
@@ -25,10 +27,14 @@ int _strlen_recursion(char *s)
 int check_palindrome(char *s, int start, int end)
 {
 	if (start >= end)
+    {
         return 1;
+    }
 
     if (s[start] != s[end])
+    {
         return 0;
+    }
 
     return (check_palindrome(s,start + 1,end - 1));
 }
@@ -43,8 +49,10 @@ int is_palindrome(char *s)
 {
 	int len = _strlen_recursion(s);
 
-    if (len == 0)
+    if (*s == '\0')
+    {
         return 1;
+    }
 
     return (check_palindrome(s,0,len - 1));
 }
