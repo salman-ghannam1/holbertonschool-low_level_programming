224d1f6467206242c7604af82440b06317dfb382
salman_ghannam1 <sloome222@gmail.com>
2025-11-09 05:27:48 +0300

    f

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -11,7 +11,7 @@
   *
   * Return: Nothing
   */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(dog_t *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
--- structures_typedef/main.c
+++ structures_typedef/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "dog.h"
 #include "main.h"
+
 /**
  * main - check the code
  *
