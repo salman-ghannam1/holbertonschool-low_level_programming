80259e56f467f8ca3e615edfe68076cdbe9b008c
salman_ghannam1 <sloome222@gmail.com>
2025-11-29 00:54:31 +0300

    f

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,44 +12,29 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *temp;
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (h == NULL)
 		return (NULL);
-
-	/* إذا كان idx = 0 → نضيف في البداية */
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
 	temp = *h;
-
-	/* نمشي للوصول إلى نود رقم idx - 1 */
-	while (temp != NULL && i < idx - 1)
-	{
+	for (i = 0; temp != NULL && i < idx - 1; i++)
 		temp = temp->next;
-		i++;
-	}
 
-	/* لو ما وصلنا للمكان الصح → ما نقدر نضيف */
 	if (temp == NULL)
 		return (NULL);
-
-	/* إذا كان الإدراج في نهاية القائمة */
 	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
 
-	/* إضافة في منتصف القائمة */
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
-
-	/* ربط الـ next والـ prev */
 	new_node->next = temp->next;
 	new_node->prev = temp;
-
-	/* تحديث الروابط للنود الحالية واللاحقة */
 	temp->next->prev = new_node;
 	temp->next = new_node;
 
