8d03715ea467ad328eb545a004415cbdd5bb8f7a
salman_ghannam1 <sloome222@gmail.com>
2025-12-01 22:36:25 +0300

    f

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -40,6 +40,30 @@ void error_close(int fd)
 }
 
 /**
+ * copy_file - copies content from one fd to another
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ * @file_from: source file name (for error messages)
+ * @file_to: destination file name (for error messages)
+ */
+void copy_file(int fd_from, int fd_to,
+	       const char *file_from, const char *file_to)
+{
+	char buffer[BUFFER_SIZE];
+	ssize_t r, w;
+
+	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		w = write(fd_to, buffer, r);
+		if (w == -1 || w != r)
+			error_write(file_to);
+	}
+
+	if (r == -1)
+		error_read(file_from);
+}
+
+/**
  * main - copies the content of a file to another file
  * @argc: argument count
  * @argv: argument vector
@@ -49,8 +73,6 @@ void error_close(int fd)
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
-	ssize_t r, w;
-	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
@@ -64,34 +86,17 @@ int main(int argc, char *argv[])
 		error_read(argv[1]);
 
 	fd_to = open(argv[2],
-		O_WRONLY | O_CREAT | O_TRUNC, 0664);
+		     O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		close(fd_from);
 		error_write(argv[2]);
 	}
 
-	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		w = write(fd_to, buffer, r);
-		if (w == -1 || w != r)
-		{
-			close(fd_from);
-			close(fd_to);
-			error_write(argv[2]);
-		}
-	}
-
-	if (r == -1)
-	{
-		close(fd_from);
-		close(fd_to);
-		error_read(argv[1]);
-	}
+	copy_file(fd_from, fd_to, argv[1], argv[2]);
 
 	if (close(fd_from) == -1)
 		error_close(fd_from);
-
 	if (close(fd_to) == -1)
 		error_close(fd_to);
 
