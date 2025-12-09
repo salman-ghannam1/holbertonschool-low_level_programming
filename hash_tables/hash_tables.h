3b7af6b56f04cf55bd88be1f98ebe7b7a8251c64
salman_ghannam1 <sloome222@gmail.com>
2025-12-09 12:03:37 +0300

    f

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,34 +1,32 @@
-#ifndef hash_tables_h
-#define hash_tables_h
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
 /**
  * struct hash_node_s - Node of a hash table
  *
- * @key: The key, string
- * The key is unique in the HashTable
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
+ * @key: The key, string (unique)
+ * @value: The value corresponding to the key
+ * @next: A pointer to the next node in the chain
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+    char *key;
+    char *value;
+    struct hash_node_s *next;
 } hash_node_t;
 
 /**
  * struct hash_table_s - Hash table data structure
  *
  * @size: The size of the array
- * @array: An array of size @size
- * Each cell of this array is a pointer to the first node of a linked list,
- * because we want our HashTable to use a Chaining collision handling
+ * @array: An array of pointers to linked list heads (chaining)
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+    unsigned long int size;
+    hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
 
-#endif //hash_tables.h
+#endif /* HASH_TABLES_H */
